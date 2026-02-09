09b182d0b82690b3b4967f42d291017e2894b18a
Mattia Mion <mattia.mion@gmail.com>
2026-02-09 15:22:33 +0100

    creo cartella controlles e file postsController per gestire logiche funzioni

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,2 +1,23 @@
 //importo sita posts da posts.js
 const listaPosts = require('../data/postsList')
+
+function index(req, res) {
+// copiamo la logica dell'index
+}
+function show(req, res) {
+// copiamo la logica della show
+}
+function store(req, res) {
+// copiamo la logica della store
+}
+function modify(req, res) {
+// copiamo la logica dell'modify
+}
+function modify(req, res) {
+// copiamo la logica dell'modify
+}
+function destroy(req, res) {
+// copiamo la logica della destroy..
+}
+// esportiamo tutto
+module.exports = { index, show, store, modify, destroy }
