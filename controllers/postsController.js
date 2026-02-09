cf0f39d243e304920160bc8f77d2e4ad0f8b242a
Mattia Mion <mattia.mion@gmail.com>
2026-02-09 15:45:05 +0100

    importo le funzioni dicharatate sul file delle rotte e testo su postman (creo anche una collection per velocizare le chiamate ai vai points)

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,22 +2,29 @@
 const listaPosts = require('../data/postsList')
 
 function index(req, res) {
-// copiamo la logica dell'index
+    res.json({ numeroPosts: listaPosts.length, listaPosts });
 }
 function show(req, res) {
-// copiamo la logica della show
+    //rendo utilizzabile singolo post usando id
+    const postById = parseInt(req.params.id) //uso parse int perche req.params.id mi sertitusce una stringa e io ho bisogna di un numero per rendere vera l'uguaglianza stretta (non ci sarei mai arrivato da solo!)
+    //cerco post specifico usando metodo 'find' e usando id specifico recuerparo con req.params.id
+    const showById = listaPosts.find((post) => post.id === postById) //sintasssi meootdo find copiata da mdn
+    if (!showById) { //SE  showbyid non esiste NOT ritorna messaggio di errore
+        return res.status(404).json({ message: 'prodotto non trovato' });
+    }
+    res.json(showById);
 }
 function store(req, res) {
-// copiamo la logica della store
+    res.send('Creazione nuovo post'); //da questa rotta di crud uso res.send perche mi restitusce un messaggio/html, sopra uso res.json perche mi deve tornare un oggetto json
+}
+function update(req, res) {
+    res.send('Modifica integrale del post ' + req.params.id);
 }
 function modify(req, res) {
-// copiamo la logica dell'modify
-}
-function modify(req, res) {
-// copiamo la logica dell'modify
+    res.send('Modifica parziale del post ' + req.params.id);
 }
 function destroy(req, res) {
-// copiamo la logica della destroy..
+    res.send('Eliminazione del post ' + req.params.id);
 }
 // esportiamo tutto
-module.exports = { index, show, store, modify, destroy }
+module.exports = { index, show, store, update, modify, destroy }
--- routes/posts.js
+++ routes/posts.js
@@ -4,39 +4,23 @@ const express = require('express');
 const router = express.Router();
 //importo sita posts da posts.js
 const listaPosts = require('../data/postsList')
+//importo il controller della risorsa posts
+const postController = require('./../controllers/postsController');
 
-//rotte di crud
+
+//rotte di crud a cui assegno come parametro della funzione la rotta che mi gestisce la logica
 // index
-router.get('/', function (req, res) {
-    res.json({ numeroPosts: listaPosts.length, listaPosts });
-});
+router.get('/', postController.index)
 // show
-router.get('/:id', function (req, res) {
-    //rendo utilizzabile singolo post usando id
-    const postById = parseInt(req.params.id) //uso parse int perche req.params.id mi sertitusce una stringa e io ho bisogna di un numero per rendere vera l'uguaglianza stretta (non ci sarei mai arrivato da solo!)
-    //cerco post specifico usando metodo 'find' e usando id specifico recuerparo con req.params.id
-    const showById = listaPosts.find((post) => post.id === postById) //sintasssi meootdo find copiata da mdn
-    if (!showById) { //SE  showbyid non esiste NOT ritorna messaggio di errore
-        return res.status(404).json({ message: 'prodotto non trovato' }); 
-    }
-    res.json(showById);
-});
+router.get('/:id', postController.show)
 // store
-router.post('/', function (req, res) {
-    res.send('Creazione nuovo post'); //da questa rotta di crud uso res.send perche mi restitusce un messaggio/html, sopra uso res.json perche mi deve tornare un oggetto json
-});
+router.post('/', postController.store)
 // update
-router.put('/:id', function (req, res) {
-    res.send('Modifica integrale del post ' + req.params.id);
-});
+router.put('/:id', postController.update)
 // modify
-router.patch('/:id', function (req, res) {
-    res.send('Modifica parziale del post ' + req.params.id);
-});
+router.patch('/:id', postController.modify)
 // destroy
-router.delete('/:id', function (req, res) {
-    res.send('Eliminazione del post ' + req.params.id);
-});
+router.delete('/:id', postController.destroy)
 
 //esporto istanza rootte
 module.exports = router;
